bc0259c01263f576cf348bc192d00ae0a4a046b8
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-03-13 20:55:02 +0200

    clear

--- server.js
+++ server.js
@@ -1,16 +1,61 @@
 /*eslint-disable*/
-const express = require('express');
-const serveStatic = require('serve-static');
-const path = require('path');
-const dotenv = require('dotenv');
-
-dotenv.config();
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 
-app.use('/', serveStatic(path.join(__dirname, '/')));
+dotenv.config();
+app.use(cors());
 
-app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", (req, res) => {
+  const documentation = [
+    {
+      url: "/info",
+      type: "GET",
+    },
+    {
+      url: "/homeCarousel",
+      type: "GET",
+    },
+  ];
+  res.send(documentation);
+});
+
+app.get("/info", (req, res) => {
+  res.send({
+    homeCarousel: [
+      {
+        id: 1,
+        imgBg:
+          "https://res.cloudinary.com/dirbnpgsp/image/upload/v1645639669/Movies%20App/Tv_Show_jdykim.jpg",
+        title: "title",
+        subTitle: "subTitle",
+        showBtn: "showBtn",
+        seeDetailsBtn: "seeDetailsBtn",
+      },
+    ],
+    masedge: [
+      {
+        id: 1,
+        title: "title",
+        subTitle: "subTitle",
+      },
+    ],
+  });
+});
+
+app.get("/homeCarousel", (req, res) => {
+  res.send({
+    id: 1,
+    imgBg:
+      "https://res.cloudinary.com/dirbnpgsp/image/upload/v1645639669/Movies%20App/Tv_Show_jdykim.jpg",
+    title: "title",
+    subTitle: "subTitle",
+    showBtn: "showBtn",
+    seeDetailsBtn: "seeDetailsBtn",
+  });
 });
 
 const port = process.env.PORT || 8080;
