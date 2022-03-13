f870d177360a7a175f69c6ad30a9765237d07b49
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-03-14 00:04:34 +0200

    edite

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-/*eslint-disable*/
 const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
 
-dotenv.config();
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -36,28 +31,21 @@ app.get("/info", (req, res) => {
         seeDetailsBtn: "seeDetailsBtn",
       },
     ],
-    masedge: [
-      {
-        id: 1,
-        title: "title",
-        subTitle: "subTitle",
-      },
-    ],
   });
 });
 
 app.get("/homeCarousel", (req, res) => {
-  res.send({
-    id: 1,
-    imgBg:
-      "https://res.cloudinary.com/dirbnpgsp/image/upload/v1645639669/Movies%20App/Tv_Show_jdykim.jpg",
-    title: "title",
-    subTitle: "subTitle",
-    showBtn: "showBtn",
-    seeDetailsBtn: "seeDetailsBtn",
-  });
+  res.send([
+    {
+      id: 1,
+      imgBg:
+        "https://res.cloudinary.com/dirbnpgsp/image/upload/v1645639669/Movies%20App/Tv_Show_jdykim.jpg",
+      title: "title",
+      subTitle: "subTitle",
+      showBtn: "showBtn",
+      seeDetailsBtn: "seeDetailsBtn",
+    },
+  ]);
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port);
-console.log(`app is running on: http://localhost:${port}`);
+app.listen(8000, () => console.log("Listening on port 8000..."));
