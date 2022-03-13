9cf07e6b6303d1070ae61b151bb42c4f634b3185
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-03-13 20:42:44 +0200

    add server.js and cors

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // fetch("http://localhost:3000/homeCarouse")
-fetch("https://reqres.in/api/users")
+fetch("http://localhost:3000/info")
   .then((res) => res.json())
   .then((data) => console.log(data))
   .catch((error) => console.log("ERROR"));
